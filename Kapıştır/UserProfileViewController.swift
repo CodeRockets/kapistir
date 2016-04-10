0bc85e50bf16e68844a5e352d2818305a899c70a
Evren Yortuçboylu <yortucboylu@gmail.com>
2016-04-10 22:36:59 +0300

    user profile page animations

--- Kapıştır/UserProfileViewController.swift
+++ Kapıştır/UserProfileViewController.swift
@@ -10,15 +10,37 @@ import UIKit
 
 class UserProfileViewController: UIViewController {
 
+    @IBOutlet weak var imProfileTop: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     
     @IBAction func close(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    override func viewDidAppear(animated: Bool) {
+       
+    }
+    
+    override func viewWillAppear(animated: Bool) {
 
+    }
+    
+    override func viewDidLayoutSubviews() {
+        UIView.animateWithDuration(
+            2.0,
+            delay: 0,
+            usingSpringWithDamping: 0.6,
+            initialSpringVelocity: 0,
+            options: .CurveLinear,
+            animations: { () -> Void in
+                
+                self.imProfileTop.constant = 66
+                self.view.layoutIfNeeded()
+                
+            }, completion: nil)
+    }
 }
