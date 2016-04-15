e0109d84b0a80dc3e14c7ab1ca315aabbaa43032
Evren Yortuçboylu <yortucboylu@gmail.com>
2016-04-16 01:11:25 +0300

    upload cropped images with animation

--- Kapıştır/CreateViewController.swift
+++ Kapıştır/CreateViewController.swift
@@ -19,6 +19,22 @@ class CreateViewController:
     
     var target = 0
     
+    @IBOutlet weak var loaderLeftHeight: NSLayoutConstraint!
+    
+    @IBOutlet weak var loaderRightHeight: NSLayoutConstraint!
+    
+    @IBOutlet weak var lblLoaderLeftBottom: NSLayoutConstraint!
+    
+    @IBOutlet weak var lblLoaderRightBottom: NSLayoutConstraint!
+    
+    @IBOutlet weak var viewLoaderLeft: UIVisualEffectView!
+    
+    @IBOutlet weak var viewLoaderRight: UIVisualEffectView!
+    
+    @IBOutlet weak var viewLeft: UIView!
+    
+    @IBOutlet weak var viewRight: UIView!
+    
     @IBAction func cancel(sender: RoundedImageButton) {
         print("cancel")
         self.dismissViewControllerAnimated(true, completion: nil)
@@ -31,10 +47,11 @@ class CreateViewController:
         print("\(croppedLeft.size)")
         print("\(croppedRight.size)")
         
-
+        uploadImage(croppedLeft, target: 1)
+        uploadImage(croppedRight, target: 0)
     }
     
-    func uploadImage() {
+    func uploadImage(image:UIImage, target: Int) {
         let headers = [
             "Authorization": "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==",
             "Content-Type": "application/x-www-form-urlencoded",
@@ -43,29 +60,59 @@ class CreateViewController:
             "x-voter-installation": App.Keys.installation!
         ]
         
-        let fileURL = NSBundle.mainBundle().URLForResource("bestof15device", withExtension: "png")
+        // Create a URL in the /tmp directory
+        let imageURL = NSURL(fileURLWithPath: NSTemporaryDirectory().stringByAppendingString(String(target) + ".jpeg"))
         
-        Alamofire.upload(.POST, App.URLs.uploadImage, headers:headers,
-                         multipartFormData: { multipartFormData in
-                            multipartFormData.appendBodyPart(fileURL: fileURL!, name: "file")
+        // save image to URL
+        UIImageJPEGRepresentation(image, 1.0)?.writeToURL(imageURL, atomically: true)
+        
+        Alamofire.upload(
+            .POST,
+            App.URLs.uploadImage,
+            headers:headers,
+            multipartFormData: { multipartFormData in
+                multipartFormData.appendBodyPart(fileURL: imageURL, name: "file")
             },
-                         encodingMemoryThreshold: Manager.MultipartFormDataEncodingMemoryThreshold,
-                         encodingCompletion: { encodingResult in
-                            switch encodingResult {
-                            case .Success(let upload, _, _):
-                                upload.progress { bytesRead, totalBytesRead, totalBytesExpectedToRead in
-                                    let ratio = Double(totalBytesRead)/Double(totalBytesExpectedToRead)
-                                    print("uploaded : \(ratio*100)")
+            encodingMemoryThreshold: Manager.MultipartFormDataEncodingMemoryThreshold,
+            encodingCompletion: { encodingResult in
+                switch encodingResult {
+                case .Success(let upload, _, _):
+                    upload.progress { bytesRead, totalBytesRead, totalBytesExpectedToRead in
+                        let ratio = Double(totalBytesRead)/Double(totalBytesExpectedToRead)
+                        print(String(target) + " uploaded : \(ratio*100)")
+                        
+                        // cell.viewLeft.frame.size.height * CGFloat(question.ratioA)
+                        
+                        let height = (self.view.frame.height - 104) * CGFloat(ratio)
+                        
+                        dispatch_async(dispatch_get_main_queue()) {
+                            UIView.animateWithDuration(0.6, delay: 0.0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.0, options: UIViewAnimationOptions.CurveEaseInOut, animations: ({
+                                
+                                if target == 0 {
+                                    self.loaderLeftHeight.constant = height
+                                    self.viewLoaderLeft.layoutIfNeeded()
+                                    
+                                    self.lblLoaderLeftBottom.constant = self.loaderLeftHeight.constant - 30
+                                    self.viewLeft.layoutIfNeeded()
+                                } else{
+                                    self.loaderRightHeight.constant = height
+                                    self.viewLoaderRight.layoutIfNeeded()
+                                    
+                                    self.lblLoaderRightBottom.constant = self.loaderRightHeight.constant - 30
+                                    self.viewRight.layoutIfNeeded()
                                 }
-                                upload.responseJSON { result in
-                                    debugPrint(result)
-                                }
-                            case .Failure(let encodingError):
-                                print(encodingError)
-                            }
-        })
-        
-
+                                
+                                
+                            }), completion: nil)
+                        }
+                    }
+                    upload.responseJSON { result in
+                        print("result: \(result)")
+                    }
+                case .Failure(let encodingError):
+                    print("fail \(encodingError)")
+                }
+            })
     }
     
     func crop(image originalImage: UIImage, targetScrollView: UIScrollView) -> UIImage {
@@ -92,10 +139,6 @@ class CreateViewController:
         
         return image
     }
-    
-    @IBOutlet weak var viewLeft: UIView!
-    
-    @IBOutlet weak var viewRight: UIView!
     
     private var imageViewLeft = UIImageView()
     
@@ -158,11 +201,11 @@ class CreateViewController:
         self.imageLeft = UIImage(named: "tap")
         self.imageRight = UIImage(named: "tap")
         
-        let offsetX: CGFloat = max( (self.scrollViewLeft.bounds.size.width - self.scrollViewLeft.contentSize.width) * 0.5, 0.0)
-        let offsetY: CGFloat = max( (self.scrollViewLeft.bounds.size.height - self.scrollViewLeft.contentSize.height) * 0.5, 0.0)
+        //let offsetX: CGFloat = max( (self.scrollViewLeft.bounds.size.width - self.scrollViewLeft.contentSize.width) * 0.5, 0.0)
+        //let offsetY: CGFloat = max( (self.scrollViewLeft.bounds.size.height - self.scrollViewLeft.contentSize.height) * 0.5, 0.0)
         
-        self.imageViewLeft.center = CGPointMake(self.scrollViewLeft.contentSize.width * 0.5 + offsetX, self.scrollViewLeft.contentSize.height * 0.5 + offsetY)
-        self.imageViewRight.center = CGPointMake(self.scrollViewRight.contentSize.width * 0.5 + offsetX, self.scrollViewRight.contentSize.height * 0.5 + offsetY)
+        //self.imageViewLeft.center = CGPointMake(self.scrollViewLeft.contentSize.width * 0.5 + offsetX, self.scrollViewLeft.contentSize.height * 0.5 + offsetY)
+        //self.imageViewRight.center = CGPointMake(self.scrollViewRight.contentSize.width * 0.5 + offsetX, self.scrollViewRight.contentSize.height * 0.5 + offsetY)
     }
 
     func tappedLeft(){
